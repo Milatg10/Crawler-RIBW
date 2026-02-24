67929105b93125f64e793d9eb3695d77ec624db9
RinaHodge <rhodgefo@alumnos.unex.es>
2026-02-24 17:29:21 +0100

    Agregados métodos de comparar

--- Ocurrencia.java
+++ Ocurrencia.java
@@ -1,8 +1,9 @@
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TreeMap;
 
-public class Ocurrencia implements Serializable {
+public class Ocurrencia implements Serializable, Comparable<Ocurrencia> {
     private static final long serialVersionUID = 1L; // Buena práctica para Serializable
     
     private Integer FTG; // Total Term Frequency (TTF)
@@ -31,5 +32,29 @@ public class Ocurrencia implements Serializable {
     @Override
     public String toString() {
         return "Total: " + FTG + " | Archivos: " + tfDocs.toString();
+    }
+    
+    @Override
+    public int compareTo(Ocurrencia otra) {
+        // Compara el FTG de este objeto con el de 'otra' Ocurrencia.
+        // Devuelve:
+        // un número negativo si este.FTG < otra.FTG
+        // 0 si son iguales
+        // un número positivo si este.FTG > otra.FTG
+        return this.FTG.compareTo(otra.getFTG());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // Compara para el equivalente a "==" y "!="
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ocurrencia que = (Ocurrencia) o;
+        return Objects.equals(FTG, que.FTG);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(FTG);
     }
 }
