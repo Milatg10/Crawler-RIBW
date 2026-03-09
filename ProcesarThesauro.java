084d8a906b2577e54a329c7455ffef3f4f94611c
RinaHodge <rhodgefo@alumnos.unex.es>
2026-03-09 21:22:07 +0100

    Solo he escrito un TODO para no olvidarme de qué hacer

---
+++ ProcesarThesauro.java
@@ -0,0 +1,10 @@
+public class ProcesarThesauro {
+    
+    //TODO: Agregar estructura de datos para almacenar el thesauro (treeMap?)
+
+
+    //TODO: Agregar método para cargar el thesauro desde un archivo (tiene que ignorar las palabras con los parentesis)
+
+
+
+}
